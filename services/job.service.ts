248a4fe08f7b8fbaa05db03c70898eddfc4133ac
dhiraj7004550347-beep <dhiraj7004550347@gmail.com>
2026-09-07 13:45:49 +0530

    Clean stale files and sync V11 production code

--- services/job.service.ts
+++
@@ -1,9 +0,0 @@
-import { jobs } from "../app/data/jobs";
-
-export function getAllJobs() {
-  return jobs;
-}
-
-export function getJobById(id: number) {
-  return jobs.find((job) => job.id === id);
-}
